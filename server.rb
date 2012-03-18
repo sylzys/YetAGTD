ead469e8cad9f5feb8177a7aa02ca7e6c7f5ba9e
Syl Zys <sylzys@gmail.com>
2012-03-18 13:02:16 +0100

    content editable ok, live update/delete

--- server.rb
+++ server.rb
@@ -3,36 +3,66 @@ require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
 require 'dm-core'
-#require 'dm-migrations/adapters/dm-sqlite-adapter'
+require 'pry'
 require 'json'
-# include Mongo
-# puts __FILE__
-puts File.expand_path("..", __FILE__)
-puts "sqlite3://#{File.join(File.dirname(__FILE__), 'db', 'db.sqlite3')}"
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
+require 'sinatra/jsonp'
 
-class Note
-  include DataMapper::Resource
-  property :id, Serial
-  property :content, Text, :required => true
-  property :complete, Boolean, :required => true, :default => false
-  property :created_at, DateTime
-  property :updated_at, DateTime
-end
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/mytasks.sqlite3")
 
-DataMapper.finalize.auto_upgrade!
+class Note  
+  include DataMapper::Resource  
+  property :id, Serial  
+  property :title, Text 
+  property :content, Text
+  property :created_at, DateTime  
+  property :updated_at, DateTime  
+end  
+  
+DataMapper.finalize.auto_upgrade!  
 
+#display all notes
 get '/' do  
   @notes = Note.all :order => :id.desc  
-  @title = 'All Notes'  
-  erb :home 
+  @title = 'My Notes'  
+  #binding.pry
+  erb :home  
 end  
 
+#save new note
 post '/' do  
   n = Note.new  
+  n.title = params[:title]
   n.content = params[:content]  
   n.created_at = Time.now  
   n.updated_at = Time.now  
   n.save  
+  #binding.pry
   redirect '/'  
+end  
+
+#get note in right panel
+get '/:id' do  
+  @title = 'My Notes' 
+  @notes = Note.all :order => :id.desc  	
+  @note = Note.get params[:id]  
+  @id = params[:id]
+  #binding.pry
+  erb :home
+end  
+
+#save edited note
+post '/edit' do  
+  n = Note.get params[:id] 
+  n.title = params[:title]
+  n.content = params[:content]  
+  n.updated_at = Time.now  
+  n.save  
+ #binding.pry
+  redirect to("/")
+end  
+post '/delete' do  
+  n = Note.get params[:id] 
+  n.destroy
+ #binding.pry
+  redirect to("/")
 end  
