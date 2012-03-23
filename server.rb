ebbc19546e525e57296ea1636816a88453b7b74f
Syl Zys <sylzys@gmail.com>
2012-03-23 22:49:20 +0100

    .

--- server.rb
+++ server.rb
@@ -1,28 +1,16 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'sinatra'
-#require 'data_mapper'
 require 'dm-core'
 require 'pry'
 require 'json'
 require 'sinatra/jsonp'
 require 'dbi'
 require 'sqlite3'
-
-#DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/db/mytasks.sqlite3")
-# dbh = DBI.connect(:SQLite3, :database => ":notes:")
-# class Note  
-#   include DataMapper::Resource  
-#   property :id, Serial  
-#   property :title, Text, :lazy => false
-#   property :content, Text, :lazy => false
-#   property :created_at, DateTime  
-#   property :updated_at, DateTime  
-# end  
   
-#DataMapper.finalize.auto_upgrade!  
 db =   SQLite3::Database.new( "db/mytasks.sqlite3" )
-db.results_as_hash = true
+db.results_as_hash = true #result in array
+
 #display all notes
 get '/' do 
   @tri = params[:tri].to_i
@@ -62,35 +50,15 @@ end
 
 #save new note
 post '/' do  
- #  n = Note.new  
- # n.title = params[:title].strip.empty? ? "Note sans titre" : params[:title]
- #  n.content = params[:content]  
   created_at = Time.now.to_i
- #  n.updated_at = Time.now  
- #  n.save  
- #binding.pry
-# db.execute("INSERT INTO notes (title, content, created_at, updated_at) VALUES ('#{params['title']}', '#{params['title']}', #{created_at}, #{created_at})")
   db.execute("INSERT INTO notes (title, content, created_at, updated_at) VALUES ('#{params['title'].lstrip.rstrip}', '#{params['content'].lstrip.rstrip}', #{created_at}, #{created_at})")
   redirect '/'  
 end  
 
-#get note in right panel
-# get '/:id' do  
-#   @title = 'My Notes' 
-#   @note = db.execute( "select * from notes where id=#{params[:id]}" )	
-#   #@note = Note.get params[:id]  
-#   @id = params[:id]
-#   binding.pry
-#   erb :home
-# end  
 
 #save edited note
 post '/edit' do  
-  # n = Note.get params[:id] 
-  # n.title = params[:title]
-  # n.content = params[:content]  
-  # n.updated_at = Time.now  
-  # n.save  
+  
  db.execute("UPDATE notes SET title=? WHERE id=?;", params[:title], params[:id]);
  db.execute("UPDATE notes SET content=? WHERE id=?;", params[:content], params[:id]);
  db.execute("UPDATE notes SET updated_at=? WHERE id=?;", params[:updated_at], params[:id]);
@@ -99,9 +67,7 @@ post '/edit' do
 end  
 
 post '/delete' do  
-  # n = Note.get params[:id] 
-  # n.destroy
- #binding.pry
+ 
   db.execute("DELETE FROM notes WHERE id=?;", params[:id]);
   redirect to("/")
 end  
